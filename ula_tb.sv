060bad46b438c938fedb18f58980d270ff24d6a5
gabrielrb1 <32144610+gabrielrb1@users.noreply.github.com>
2017-09-29 00:29:31 -0300

    vs 1.0
    Primeira tentativa de finalizar o test bench

--- ula_tb.sv
+++ ula_tb.sv
@@ -1,9 +1,17 @@
-module ula_tb;
+//Testbench para o código "ula.sv"
+//Declaração de variáveis
+//Criação de um vetor "teste" para recebimento do arquivo de teste
+
+module ula_tb; 
 reg clk;
 reg [1:0] op;
 reg [1:0] a;
 reg [1:0] b;
 wire [1:0] result;
+reg [7:0] teste [100:0];
+reg i; //Definição da variável "i" para percorrer o vetor de teste
+reg [1:0] t_res; //Variável criada para receber o resultado esperado, proveniente do arquivo aberto
+
 
 ula U0(
 .clk (clk),
@@ -13,18 +21,48 @@ ula U0(
 .b (b)
 );
 
+
+//Leitura do arquivo de teste e armazenamento no vetor "teste"
+//Inicialização das variáveis de varredura err
 initial begin
 	clk = 0;
-	op <= 2'b01;
-	a <= 2'b10;
-	b <= 2'b10;
+	$readmemb("C:\intelFPGA_pro\17.0\Projeto1\Teste.txt", teste);
+	i = 0;
 	
 end
 
-always
+// Geração do clock
+always begin
 
 	#5 clk = !clk;
-		
+end
+
+
+//Após a subida positiva do clock, o programa aguarda um 1 passo e lê os valores contidos no vetor teste
+//ajustando-o de acordo com as variáveis informadas
+always @ (posedge clk) begin
+	#1;
+	{a, b, op, t_res} = teste[i];
+	
+end
+	
+always @ (negedge clk) begin
+	if (result !==t_res) begin
+		$display ("Erro para entradas: %b", a,b);
+		$display ("Saida esperada: %b; Saida obtida: %b", t_res, result);
+	end 
+	i = i+1;
+	if (i==8) begin
+		$finish;
+	end
+
+end
+
+initial begin
+	$display("a\tb\top\trestult\ti");
+	$monitor("%b,\t%b,\t%b,\t%b,\t%b,\t%d",$time,a,b,op,t_res,i);
+end
+
 
 endmodule
 
